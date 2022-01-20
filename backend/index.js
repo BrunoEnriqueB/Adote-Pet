4ef5f7a39523aab96079381654e73294354fb668
BrunoEnriqueB <brunobaronenrique@gmail.com>
2022-01-20 20:52:01 -0300

    Configurando projeto

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+// config  JSON
+app.use(express.json());
+
+// Solve CORS
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+
+// Public folder
+app.use(express.static('public'));
+
+//Routes
+
+
+app.listen(port)
