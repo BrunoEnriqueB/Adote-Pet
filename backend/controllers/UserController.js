1b218aa4e4df86fb0cb44495c9fe2b6da090ba34
BrunoEnriqueB <brunobaronenrique@gmail.com>
2022-01-20 22:56:39 -0300

    Adicionando funcionalidade de registrar usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,40 +1,58 @@
 const User = require('../models/User');
-const { Pet } = require('../models/Pet');
+const bcrypt = require('bcrypt');
 
 module.exports = class UserController {
   static createUser = async (req, res) => {
+    const { name, email, password, confirmpassword, phone } = req.body;
+
+    if (!name) {
+      return res.status(422).json({ message: "Nome obrigatório!" });
+    }
+    if (!email) {
+      return res.status(422).json({ message: "email obrigatório!" });
+    }
+    if (!password) {
+      return res.status(422).json({ message: "Senha obrigatória!" });
+    }
+    if (!confirmpassword) {
+      return res.status(422).json({ message: "confirmação de senha obrigatório!" });
+    }
+    if (!phone) {
+      return res.status(422).json({ message: "Número de telefone obrigatório!" });
+    }
+
+    if (password !== confirmpassword) {
+      return res.status(422).json({ message: "A senha e a confirmação de senha precisam ser iguais!!" });
+    }
+
+    //check if user exists
+    const userExists = await User.findOne({ email: email });
+
+    if (userExists) {
+      return res.status(422).json({ message: "Usuário com este email já existe!" });
+    }
+
+    //create cryptografe password
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    //creat a user
+    // const user = new User({
+    //   name,
+    //   email,
+    //   phone,
+    //   password: passwordHash
+    // });
+
     try {
-      const { name, email, password, confirmpassword, phone } = req.body;
+      
+      // const newUser = await user.save();
+      // da pra criar usuário assim também
+      await User.create({ name, email, phone, password: passwordHash })
 
-      if(!name) {
-        return res.status(422).json({message: "Nome obrigatório!"});
-      }
-      if(!email) {
-        return res.status(422).json({message: "email obrigatório!"});
-      }
-      if(!password) {
-        return res.status(422).json({message: "Senha obrigatória!"});
-      }
-      if(!confirmpassword) {
-        return res.status(422).json({message: "confirmação de senha obrigatório!"});
-      }
-      if(!phone) {
-        return res.status(422).json({message: "Número de telefone obrigatório!"});
-      }
-
-      if(password !== confirmpassword) {
-        return res.status(422).json({message: "A senha e a confirmação de senha precisam ser iguais!!"});
-      }
-
-      //check if user exists
-      const userExists = await User.findOne({email: email});
-
-      if(userExists) {
-        return res.status(422).json({message: "Usuário com este email já existe!"});
-      }
-
+      return res.status(201).json({ message: "Usuário criado com sucesso!" });
     } catch (error) {
-      console.log(error);
+      res.status(500).json({message: error})
     }
   }
 }
