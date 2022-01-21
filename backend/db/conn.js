943cefdb74853a442377f833a9e147050e5a5e1d
BrunoEnriqueB <brunobaronenrique@gmail.com>
2022-01-20 22:56:23 -0300

    Mudando método de conexão

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const main = async () => {
-  await mongoose.createConnection('mongodb://localhost:27017/getapet');
+  await mongoose.connect('mongodb://localhost:27017/getapet');
 }
 
 main().catch(err => console.log(err));
