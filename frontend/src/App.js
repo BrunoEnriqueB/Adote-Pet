49a04504fddaca589669c65411bb021d5d736b93
BrunoEnriqueB <brunobaronenrique@gmail.com>
2022-01-23 21:19:19 -0300

    Estruturando Navbar e o footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
 
 // pages
 import Login from './components/pages/Auth/Login';
@@ -9,11 +11,13 @@ import Home from './components/pages/Home';
 export default function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />}/>
         <Route path="/register" element={<Register />}/>
         <Route path="/" element={<Home />}/>
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        <span className="bold">Get a pet</span> &copy; 2022
+      </p>
+    </footer>
+  )
+}
