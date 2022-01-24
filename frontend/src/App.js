d4c04ebcbb8baec44ad8a6bf0baa19ae029d9e50
BrunoEnriqueB <brunobaronenrique@gmail.com>
2022-01-23 21:11:03 -0300

    Criando rotas de autenticação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+// pages
+import Login from './components/pages/Auth/Login';
+import Register from './components/pages/Auth/Register';
+import Home from './components/pages/Home';
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<Register />}/>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
